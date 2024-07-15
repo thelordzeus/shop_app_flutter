1f78b8fd72b180c2b25b25423115976240aa57c2
thelordzeus <arnav55125@gmail.com>
2024-07-15 22:41:15 +0530

    feat: added search bar and filter pills

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,21 +1,86 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const ['All', 'Addidas', 'Nike', 'Bata'];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 32,
-            fontFamily: 'Lato',
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(225, 225, 225, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(50),
       ),
     );
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  'Shoes\nCollection',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                ),
+              ),
+              //dynamically sets the width of the textfield according to the available space
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+              itemCount: filters.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                final filter = filters[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Chip(
+                    label: Text(filter),
+                    backgroundColor: const Color.fromRGBO(245, 247, 249, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    side: const BorderSide(
+                      color: Color.fromRGBO(245, 247, 249, 1),
+                    ),
+                    labelStyle: const TextStyle(fontSize: 16),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
