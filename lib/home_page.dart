3a73ef8e982fcdd6949b827f4c3fe091c261197e
thelordzeus <arnav55125@gmail.com>
2024-07-17 01:03:30 +0530

    feat: added products card

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/global_variables.dart';
+import 'package:shop_app_flutter/product_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -85,6 +87,23 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 );
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                return ProductCard(
+                  title: product['title'] as String,
+                  price: product['price'] as double,
+                  image: product['imageUrl'].toString(),
+                  backgroundColor: index.isEven
+                      ? const Color.fromRGBO(216, 240, 253, 1)
+                      : const Color.fromRGBO(245, 247, 249, 1),
+                );
+                ;
               },
             ),
           )
