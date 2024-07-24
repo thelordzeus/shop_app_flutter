77bfaffd19d35746d8c4c3c5df0f255c2ff7cf2f
thelordzeus <arnav55125@gmail.com>
2024-07-24 15:15:03 +0530

    feat: added cart section

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
+import 'package:shop_app_flutter/global_variables.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Cart Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart'),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          final cartItem = cart[index];
+
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage(
+                cartItem['imageUrl'].toString(),
+              ),
+            ),
+            title: Text(
+              cartItem['title'].toString(),
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            subtitle: Text(
+              'Size: ${cartItem['sizes']}',
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
